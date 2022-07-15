61d3a217237f25c21ac06d31f14219a2c3900ed5
Attilius <leo20@freemail.hu>
2022-07-15 21:58:38 +0200

    Added services.php

---
+++ services.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+    "responseFactory" => function(ServiceContainer $container){
+        return new ResponseFactory($container->get("viewRenderer"));
+    },
+    "viewRenderer" => function(ServiceContainer $container){
+        return new ViewRenderer($container->get("basePath"));
+    },
+    "responseEmitter" => function(){
+        return new ResponseEmitter();
+    },
+    "homeController" => function(){
+        return new Controllers\HomeController();
+    },
+    "singleImageController" => function(){
+        return new Controllers\SingleImageController();
+    },
+    "singleImageEditController" => function(){
+        return new Controllers\SingleImageEditController();
+    },
+    "singleImageDeleteController" => function(){
+        return new Controllers\SingleImageDeleteController();
+    },
+    "loginFormController" => function(){
+        return new Controllers\LoginFormController();
+    },
+    "loginSubmitController" => function(){
+        return new Controllers\LoginSubmitController();
+    },
+    "logoutSubmitController" => function(){
+        return new Controllers\LogoutSubmitController();
+    },
+    "notFoundController" => function(){
+        return new Controllers\NotFoundController();
+    },
+    "dispatcher" => function(ServiceContainer $container){
+        $dispatcher = new Dispatcher($container,'notFoundController@handle');
+
+        $dispatcher->addRoute('/', 'homeController@handle');
+        $dispatcher->addRoute('/about', 'aboutController');
+        $dispatcher->addRoute('/image/(?<id>[\d]+)', 'singleImageController@display');
+        $dispatcher->addRoute('/image/(?<id>[\d]+)/edit', 'singleImageEditController@edit', 'POST');
+        $dispatcher->addRoute('/image/(?<id>[\d]+)/delete', 'singleImageDeleteController@delete', 'POST');
+        $dispatcher->addRoute('/login', 'loginFormController@show');
+        $dispatcher->addRoute('/logout', 'logoutSubmitController@submit');
+        $dispatcher->addRoute('/login', 'loginSubmitController@submit','POST');
+
+        return $dispatcher;
+    }
+];
