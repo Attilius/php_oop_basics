6cbf6e0ae1f8fa7adc658894bcc043291677ea53
Attilius <leo20@freemail.hu>
2022-07-16 22:36:07 +0200

    Added ServiceNotFoundException.php

---
+++ core/Exception/ServiceNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Exception;
+
+use Exception;
+
+class ServiceNotFoundException extends Exception
+{
+
+    public function __construct(string $service)
+    {
+        parent::__construct("Service with name '". $service ."' is not found!");
+    }
+}
