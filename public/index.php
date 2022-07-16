6ff4461584b30061932afc1176c80085fb4762e4
Attilius <leo20@freemail.hu>
2022-07-16 22:34:06 +0200

    Modified index.php

--- public/index.php
+++ public/index.php
@@ -7,8 +7,6 @@ ini_set("display_errors", "1");
 
 
 require_once "../core/functions.php";
-require_once "../core/config.php";
-require_once "../core/controllers.php";
 require_once "../autoload.php";
 
 (new Application(new ServiceContainer(include "../services.php")))->start(realpath(__DIR__."/../")); // class member access on instantiation since php 5.4
