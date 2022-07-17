14270b5e866951237b6477e18b9a7a9c2ce3198e
Attilius <leo20@freemail.hu>
2022-07-17 22:38:35 +0200

    Modified ResponseFactory.php

--- core/ResponseFactory.php
+++ core/ResponseFactory.php
@@ -2,21 +2,22 @@
 
 class ResponseFactory
 {
-    private $viewRenderer;
+    private ViewRenderer $viewRenderer;
 
     public function __construct(ViewRenderer $viewRenderer)
     {
         $this->viewRenderer = $viewRenderer;
     }
 
-    public function createResponse($controllerResult){
+    public function createResponse($controllerResult, array $params)
+    {
         if (is_array($controllerResult)){
             if (preg_match("%^redirect\:%", $controllerResult[0])) {
                 return new Response("", [
                     "Location" => substr($controllerResult[0], 9)
                 ], 302, "Found");
             } else {
-                $modelAndView = new ModelAndView($controllerResult[0], $controllerResult[1]);
+                $modelAndView = new ModelAndView($controllerResult[0], array_merge($controllerResult[1], $params));
                 return new Response($this->viewRenderer->render($modelAndView), [], 200,"Ok");
             }
         }
