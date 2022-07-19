3e36cccc7b03e63abc49dc7f533d0d11244f209c
Attilius <leo20@freemail.hu>
2022-07-19 16:22:29 +0200

    Added DispatchingMiddleware.php

---
+++ core/Middleware/DispatchingMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Middleware;
+
+use Dispatcher;
+use Request;
+use Response;
+use ResponseFactory;
+
+class DispatchingMiddleware implements MiddlewareInterface
+{
+    private Dispatcher $dispatcher;
+    private ResponseFactory $responseFactory;
+
+    public function __construct(Dispatcher $dispatcher, ResponseFactory $responseFactory)
+    {
+        $this->dispatcher = $dispatcher;
+        $this->responseFactory = $responseFactory;
+    }
+
+    public function process(Request $request, Response $response, callable $next)
+    {
+        $controllerResult = $this->dispatcher->dispatch($request);
+        return $this->responseFactory->createResponse($controllerResult, $request->getSession()->toArray());
+    }
+}
