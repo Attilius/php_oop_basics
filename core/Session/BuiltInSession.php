c4914fd97873af971de9e2a80ad664df4537077c
Attilius <leo20@freemail.hu>
2022-07-19 19:40:07 +0200

    Added BuiltInSession.php

---
+++ core/Session/BuiltInSession.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Session;
+
+
+class BuiltInSession implements SessionInterface
+{
+
+    public function has($key)
+    {
+        return array_key_exists($key, $_SESSION);
+    }
+
+    public function get($key)
+    {
+        return $_SESSION[$key];
+    }
+
+    public function put($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function remove($key)
+    {
+        unset($_SESSION[$key]);
+    }
+
+    public function clear()
+    {
+        unset($_SESSION);
+    }
+
+    public function toArray()
+    {
+        return $_SESSION;
+    }
+}
