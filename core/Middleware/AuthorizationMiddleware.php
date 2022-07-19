4a5b5dbafa25b2c89cc03b948722ee3d0ed21729
Attilius <leo20@freemail.hu>
2022-07-19 16:21:05 +0200

    Added AuthorizationMiddleware.php

---
+++ core/Middleware/AuthorizationMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Middleware;
+
+use Request;
+use Response;
+use Services\AuthService;
+
+class AuthorizationMiddleware implements MiddlewareInterface
+{
+
+    private array $protectedUrls;
+    private AuthService $authService;
+    private string $loginUrl;
+
+    /**
+     * AuthorizationMiddleware constructor
+     * @param string[] $protectedUrls
+     * @param AuthService $authService
+     * @param string $loginUrl
+     */
+    public function __construct(array $protectedUrls, AuthService $authService, string $loginUrl)
+    {
+        $this->protectedUrls = $protectedUrls;
+        $this->authService = $authService;
+        $this->loginUrl = $loginUrl;
+    }
+
+    public function process(Request $request, Response $response, callable $next)
+    {
+        if (in_array($request->getUri(), $this->protectedUrls) && !$this->authService->check()){
+            return Response::redirect($this->loginUrl);
+        }
+        return $next($request, $response);
+    }
+}
