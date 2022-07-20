384c038504cf6c0eec220d5d7228944b09ba9e72
Attilius <leo20@freemail.hu>
2022-07-20 09:49:15 +0200

    Added ForgotPasswordController.php

---
+++ app/Controllers/ForgotPasswordController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Controllers;
+
+use Session\SessionInterface;
+
+class ForgotPasswordController
+{
+    private SessionInterface $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    public function show()
+    {
+        return [
+            "forgotpass",
+            [
+                "sent" => $this->sent(),
+                "title" => "Forgot password"
+            ]
+        ];
+    }
+
+    private function sent()
+    {
+        $sentPassword = $this->session->has("sentPassword");
+        $this->session->remove("sentPassword");
+        return $sentPassword;
+    }
+}
