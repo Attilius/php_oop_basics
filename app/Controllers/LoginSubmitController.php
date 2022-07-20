8f65531c8e57fb7d2828509711aad56e076729bb
Attilius <leo20@freemail.hu>
2022-07-20 09:51:49 +0200

    Added LoginSubmitController.php

---
+++ app/Controllers/LoginSubmitController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Controllers;
+
+use Services\AuthService;
+use Session\SessionInterface;
+
+class LoginSubmitController
+{
+    private AuthService $authService;
+    private SessionInterface $session;
+
+    public function __construct(AuthService $authService, SessionInterface $session)
+    {
+        $this->authService = $authService;
+        $this->session = $session;
+    }
+
+    function submit()
+    {
+        $password = trim($_POST["password"]);
+        $email = trim($_POST["email"]);
+        $success = $this->authService->loginUser($email, $password);
+        if ($success){
+            $view = "redirect:/";
+        }else{
+            $this->markAsLoginFailed();
+            $view = "redirect:/login";
+        }
+        return [
+            $view,
+            []
+        ];
+    }
+
+    private function markAsLoginFailed()
+    {
+        $this->session->put("containsError", 1);
+    }
+}
