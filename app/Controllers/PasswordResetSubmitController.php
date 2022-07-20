ffeec6143ca3e204e6eaaf210139efa0345b4e41
Attilius <leo20@freemail.hu>
2022-07-20 09:56:02 +0200

    Added PasswordResetSubmitController.php

---
+++ app/Controllers/PasswordResetSubmitController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Controllers;
+
+use Request;
+use Services\ForgotPasswordService;
+
+class PasswordResetSubmitController
+{
+
+    private Request $request;
+    private ForgotPasswordService $service;
+
+    public function __construct(Request $request, ForgotPasswordService $service)
+    {
+        $this->request = $request;
+        $this->service = $service;
+    }
+
+    public function submit()
+    {
+        $params = $this->request->getParams();
+        $session = $this->request->getSession();
+        $valid = $this->validate($params);
+        if ($valid){
+            $this->service->updatePassword($params["token"], $params["password"]);
+            $session->put("resetPassword", true);
+            return [
+                "redirect:/reset"
+            ];
+        } else {
+            $session->put("failed", true);
+            return [
+                "redirect:/reset"
+            ];
+        }
+    }
+
+    private function validate($params)
+    {
+        $password = $params["password"];
+        $passwordConf = $params["password_confirmation"];
+        return $password == $passwordConf && strlen($password) >= 8 && $this->service->checkTokenExist($params["token"]);
+    }
+}
