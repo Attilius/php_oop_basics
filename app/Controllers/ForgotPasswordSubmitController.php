d340a49af856fb66ef4dbde7528dbc19554bab8b
Attilius <leo20@freemail.hu>
2022-07-20 09:49:44 +0200

    Added ForgotPasswordSubmitController.php

---
+++ app/Controllers/ForgotPasswordSubmitController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Controllers;
+
+use Request;
+use Services\ForgotPasswordService;
+
+class ForgotPasswordSubmitController
+{
+
+    private Request $request;
+    private ForgotPasswordService $service;
+
+    public function __construct(Request $request, ForgotPasswordService $service)
+    {
+        $this->request = $request;
+        $this->service = $service;
+    }
+
+    public function submit()
+    {
+        $this->markForgotPasswordSent();
+        $params = $this->request->getParams();
+        $this->service->forgotPassword($params["email"]);
+        return [
+            "redirect:/forgotpass",
+            []
+        ];
+    }
+
+    private function markForgotPasswordSent()
+    {
+        $this->request->getSession()->put("sentPassword", 1);
+    }
+}
