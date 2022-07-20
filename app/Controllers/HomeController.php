d668ae3c5814f13aaa027208125efa0464ed5cb5
Attilius <leo20@freemail.hu>
2022-07-20 09:50:09 +0200

    Added HomeController.php

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Controllers;
+
+use Services\PhotoService;
+
+class HomeController
+{
+    private PhotoService $photoService;
+
+    public function __construct(PhotoService $photoService)
+    {
+        $this->photoService = $photoService;
+    }
+
+    function handle()
+    {
+        $size = $_GET['size'] ?? 10; // (??) if $_GET['size] = null -> default value = 10
+        $page = $_GET['page'] ?? 1;
+        $total = $this->photoService->getTotal();
+        $offset = ($page - 1) * $size;
+        $content = $this->photoService->getPhotosPaginated($size, $offset);
+        $possiblePageSizes = [10,20,30,40,50];
+
+        return [
+            "home",
+            [
+                "title" => "Home",
+                "content" => $content,
+                "total" => $total,
+                "size" => $size,
+                "page" => $page,
+                "offset" => $offset,
+                "possiblePageSizes" => $possiblePageSizes
+            ]
+        ];
+    }
+}
