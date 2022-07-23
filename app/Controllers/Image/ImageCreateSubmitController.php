17605d8215d3c8e57a512892ae5b9b3aefe46856
Attilius <leo20@freemail.hu>
2022-07-23 18:30:01 +0200

    Modified ImageCreateSubmitController.php

--- app/Controllers/Image/ImageCreateSubmitController.php
+++ app/Controllers/Image/ImageCreateSubmitController.php
@@ -3,6 +3,7 @@
 namespace Controllers\Image;
 
 use Request\Request;
+use Response\Redirect;
 use RuntimeException;
 use Services\PhotoService;
 use Validation\Validator;
@@ -29,50 +30,28 @@ class ImageCreateSubmitController
         try {
             $title = $this->request->getParam("title");
             $file = $this->request->getFile("file");
-            $violations = $this->validate($this->request);
+            $violations = $this->validate($title, $file);
             if (count($violations) !== 0){
-                $this->request->getSession()->put("violations", $violations);
-                return [
-                    "redirect:/image/add",
-                    []
-                ];
+                return Redirect::to("/image/add")
+                    ->with("violations", $violations);
             }
-            switch ($file->getError()){
-                case UPLOAD_ERR_OK:
-                    break;
-                case UPLOAD_ERR_NO_FILE:
-                    throw new RuntimeException("No file sent.");
-                case UPLOAD_ERR_INI_SIZE:
-                case UPLOAD_ERR_FORM_SIZE:
-                    throw new RuntimeException("Exceeded filesize limit.");
-                default:
-                    throw new RuntimeException("Unknown errors.");
-            }
-            $check = getimagesize($file->getTemporaryName());
-            if ($check !== false){
-                $targetFile = uniqid($targetDir, true); //$targetDir. basename($file->getName());
-                $file->moveTo($targetFile);
-                $photo = $this->photoService->createImage($title, "/private/".basename($targetFile));
-                return [
-                    "redirect:/image/". $photo->getId(),
-                    []
-                ];
-            } else {
-                throw new RuntimeException("File is not an image!");
-            }
+
+            $targetFile = uniqid($targetDir, true); //$targetDir. basename($file->getName());
+            $file->moveTo($targetFile);
+            $photo = $this->photoService->createImage($title, "/private/".basename($targetFile));
+            return Redirect::to("/image/add". $photo->getId());
+
         } catch (RuntimeException $ex) {
             logMessage("ERROR", $ex->getMessage() );
-            return [
-                "redirect:/image/add",
-                []
-            ];
+            return Redirect::to("/image/add");
         }
     }
 
-    private function validate(Request $request)
+    private function validate($title, $file)
     {
         return $this->validator->validate([
-            $request->getParam("title") => "required|min:5|max:255"
+            "required|min:5|max:255" => $title,
+            "required|image" => $file
         ]);
     }
 }
