982e78bc0f2aaa68e384e1bb70e3c4923de7770a
Attilius <leo20@freemail.hu>
2022-07-25 20:30:23 +0200

    Modified ViewRenderer.php

--- core/ViewRenderer.php
+++ core/ViewRenderer.php
@@ -1,19 +1,32 @@
 <?php
 
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+
 class ViewRenderer
 {
     private $basePath;
+    private CsrfTokenManagerInterface $csrfTokenManager;
 
-    public function __construct($basePath)
+    public function __construct($basePath, CsrfTokenManagerInterface $csrfTokenManager)
     {
         $this->basePath = $basePath;
+        $this->csrfTokenManager = $csrfTokenManager;
     }
 
-    public function render(ModelAndView $modelAndView){
+    public function render(ModelAndView $modelAndView)
+    {
         extract($modelAndView->getModel());
         $view = $modelAndView->getViewName();
+        $_csrf = $this->generateCsrfInput();
         ob_clean();
         require_once $this->basePath."/templates/layout.php";
         return ob_get_clean();
     }
+
+    private function generateCsrfInput()
+    {
+        $csrfToken = $this->csrfTokenManager->getToken("_csrf")->getValue();
+        return "<input type='hidden' name='_csrf' value='${csrfToken}'/>";
+    }
+
 }
