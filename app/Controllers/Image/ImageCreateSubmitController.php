c726c08e4ea431fbe04293d125996334521b998b
Attilius <leo20@freemail.hu>
2022-07-25 20:28:21 +0200

    Modified ImageCreateSubmitController.php

--- app/Controllers/Image/ImageCreateSubmitController.php
+++ app/Controllers/Image/ImageCreateSubmitController.php
@@ -3,7 +3,7 @@
 namespace Controllers\Image;
 
 use Request\Request;
-use Response\Redirect;
+use Response\Response;
 use RuntimeException;
 use Services\PhotoService;
 use Validation\Validator;
@@ -32,18 +32,24 @@ class ImageCreateSubmitController
             $file = $this->request->getFile("file");
             $violations = $this->validate($title, $file);
             if (count($violations) !== 0){
-                return Redirect::to("/image/add")
-                    ->with("violations", $violations);
+                return new Response(json_encode([
+                    "success" => false,
+                    "error" => $violations->get(0)->getMessage()
+                ]), [], 400, "Bad request");
             }
-
-            $targetFile = uniqid($targetDir, true); //$targetDir. basename($file->getName());
+            $targetFile = uniqid($targetDir, true);
             $file->moveTo($targetFile);
             $photo = $this->photoService->createImage($title, "/private/".basename($targetFile));
-            return Redirect::to("/image/add". $photo->getId());
-
+            return new Response(json_encode([
+                "success" => true,
+                "message" => "/image/". $photo->getId()
+            ]), [], 201, "Created");
         } catch (RuntimeException $ex) {
             logMessage("ERROR", $ex->getMessage() );
-            return Redirect::to("/image/add");
+            return new Response(json_encode([
+                "success" => false,
+                "error" => $ex->getMessage()
+            ]), [], 500, "Internal server error");
         }
     }
 
