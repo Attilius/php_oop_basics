bae3ada04169105ef7375a7aaa61b3ce6ff12a44
Attilius <leo20@freemail.hu>
2022-07-25 20:36:49 +0200

    Modified ResponseEmitter.php

--- core/Response/ResponseEmitter.php
+++ core/Response/ResponseEmitter.php
@@ -4,14 +4,14 @@ namespace Response;
 
 class ResponseEmitter
 {
-    public function emit(ResponseInterface $response)
+    public function emit(ResponseInterface $response): void
     {
         $this->emitStatusLine($response->getStatusCode(), $response->getReasonPhrase());
         $this->emitHeaders($response->getHeaders());
         $response->emitBody();
     }
 
-    private function emitStatusLine(int $statusCode, string $reasonPhrase)
+    private function emitStatusLine(int $statusCode, string $reasonPhrase): void
     {
         header(sprintf(
             "HTTP/1.1 %d%s",
@@ -20,7 +20,7 @@ class ResponseEmitter
         ), true, $statusCode);
     }
 
-    private function emitHeaders(array $headers)
+    private function emitHeaders(array $headers): void
     {
         foreach ($headers as $key => $value){
             header(sprintf("%s: %s", $key,$value));
