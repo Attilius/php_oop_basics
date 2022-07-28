fca96e8790999e656d97cff5a5bc3dbbc8ebfaed
Attilius <leo20@freemail.hu>
2022-07-28 18:52:27 +0200

    Modified Response.php

--- core/Response/Response.php
+++ core/Response/Response.php
@@ -46,7 +46,8 @@ class Response implements ResponseInterface
         return $this->reasonPhrase;
     }
 
-    public static function redirect($url){
+    public static function redirect($url)
+    {
         return new Response("",[
             "Location" => $url
         ], 302, "Found");
