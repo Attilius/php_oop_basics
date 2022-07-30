7c2c2daaa6274ff9496ba4ca982e805abfcbf0ad
Attilius <leo20@freemail.hu>
2022-07-30 21:55:11 +0200

    Modified forgotpass.php

--- templates/forgotpass.php
+++ templates/forgotpass.php
@@ -2,15 +2,15 @@
     <form method="post" action="/forgotpass">
         <?php if ($sent): ?>
             <div class="alert alert-success">
-                We sent an e-mail to the address if it matches an account in our database.
+                <?= $trans("We sent an e-mail to the address if it matches an account in our database") . "."; ?>
             </div>
-            <a role="button" class="btn btn-info" href="/login">Back to login</a>
+            <a role="button" class="btn btn-info" href="/login"><?= $trans("Back to login"); ?></a>
         <?php else: ?>
             <div class="form-group">
-                <label for="email">Email address</label>
-                <input type="email" class="form-control" name="email" id="email" placeholder="Enter your email address...">
+                <label for="email"><?= $trans("Email address"); ?></label>
+                <input type="email" class="form-control" name="email" id="email" placeholder="<?= $trans("Enter your email address") . "..."; ?>">
             </div>
-            <button type="submit" class="btn btn-primary mt-2">Reset password</button>
+            <button type="submit" class="btn btn-primary mt-2"><?= $trans("Reset password"); ?></button>
         <?php endif; ?>
     </form>
 </main>
