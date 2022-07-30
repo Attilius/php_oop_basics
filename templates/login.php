0f9ee256dd45792aef06839b644c2025446d20b0
Attilius <leo20@freemail.hu>
2022-07-30 21:56:58 +0200

    Modified login.php

--- templates/login.php
+++ templates/login.php
@@ -2,22 +2,22 @@
     <form method="post" action="/login">
         <?php if ($containsError):?>
         <div class="alert alert-danger">
-            The username or password aren't matching!
+            <?= $trans("The username or password aren't matching") . "!"; ?>
         </div>
         <?php endif; ?>
         <div class="form-group">
-            <label for="email">Email address</label>
-            <input class="form-control" type="email" name="email" id="email" placeholder="Enter your email address...">
+            <label for="email"><?= $trans("Email address"); ?></label>
+            <input class="form-control" type="email" name="email" id="email" placeholder="<?= $trans("Enter your email address") . "..."; ?>">
         </div>
         <div class="form-group">
-            <label for="password">Password</label>
-            <input class="form-control" type="password" name="password" id="password" placeholder="Enter your password...">
+            <label for="password"><?= $trans("Password"); ?></label>
+            <input class="form-control" type="password" name="password" id="password" placeholder="<?= $trans("Enter your password") . "..."; ?>">
         </div>
-        <button class="btn btn-primary mt-2" type="submit">Login</button>
+        <button class="btn btn-primary mt-2" type="submit"><?= $trans("Login"); ?></button>
         <?= $_csrf ?>
     </form>
     <div style="padding-top: 20px; text-align: center">
-        <a class="btn btn-info" role="button" href="/register">Register</a>
-        <a class="btn btn-info" role="button"href="/forgotpass">Forgot password</a>
+        <a class="btn btn-info" role="button" href="/register"><?= $trans("Register"); ?></a>
+        <a class="btn btn-info" role="button" href="/forgotpass"><?= $trans("Forgot password"); ?></a>
     </div>
 </main>
