2b4f1a7d1aa888110fbc577b285aa6dd549c54a5
Attilius <leo20@freemail.hu>
2022-08-02 20:55:07 +0200

    Modified MiddlewareInterface.php

--- core/Middleware/MiddlewareInterface.php
+++ core/Middleware/MiddlewareInterface.php
@@ -7,5 +7,10 @@ use Response\Response;
 
 interface MiddlewareInterface
 {
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param callable $next
+     */
     public function process(Request $request, Response $response, callable $next);
 }
