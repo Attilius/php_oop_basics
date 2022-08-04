5e92ebd712fe608aa4f0c60664a6187f4c5194be
Attilius <leo20@freemail.hu>
2022-08-04 21:06:50 +0200

    Modified Validator.php

--- core/Validation/Validator.php
+++ core/Validation/Validator.php
@@ -2,6 +2,7 @@
 
 namespace Validation;
 
+use InvalidArgumentException;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -42,17 +43,12 @@ class Validator
     private function createConstraint(string $identifier): Constraint
     {
         list($constraint, $param) = array_pad(explode(":", $identifier), 2, null);
-        switch ($constraint){
-            case "required" :
-                return new NotBlank();
-            case "min" :
-                return new Length(["min" => $param]);
-            case "max" :
-                return new Length(["max" => $param]);
-            case "image":
-                return new Image();
-            default:
-                throw new \InvalidArgumentException("The constraint string ${$constraint} is not available!");
-        }
+        return match ($constraint) {
+            "required" => new NotBlank(),
+            "min" => new Length(["min" => $param]),
+            "max" => new Length(["max" => $param]),
+            "image" => new Image(),
+            default => throw new InvalidArgumentException("The constraint string ${$constraint} is not available!"),
+        };
     }
 }
