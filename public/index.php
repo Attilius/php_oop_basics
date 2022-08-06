7477d808f4126539143e26b5276de2517d6ae124
Attilius <leo20@freemail.hu>
2022-08-06 22:28:54 +0200

    Modified index.php

--- public/index.php
+++ public/index.php
@@ -5,8 +5,17 @@ session_start();
 error_reporting(E_ALL);
 ini_set("display_errors", "1");
 
-
 require_once "../core/functions.php";
 require_once "../vendor/autoload.php";
+
+//$locale = "hu_HU.utf8";
+//
+//if (defined("LC_MESSAGES")){
+//    setlocale(LC_MESSAGES, $locale);
+//    bindtextdomain("messages", "../i18n");
+//} else {
+//    putenv("LC_ALL={$locale}");
+//    bindtextdomain("messages", "..\i18n");
+//}
 
 (new Application(new ServiceContainer(include "../services.php")))->start(realpath(__DIR__."/../")); // class member access on instantiation since php 5.4
