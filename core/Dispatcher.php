fd5898b3669a3de846b4706bd9ed466eb82b4f65
Attilius <leo20@freemail.hu>
2022-08-18 20:25:44 +0200

    Modified Dispatcher.php

--- core/Dispatcher.php
+++ core/Dispatcher.php
@@ -18,6 +18,7 @@ class Dispatcher
     /**
      * @param $action
      * @param $callable
+     * @param $method
      * @return void
      */
     public function addRoute($action, $callable, $method = "GET"): void
