619902db46d152268e1584548de318a77aa7a404
Attilius <leo20@freemail.hu>
2022-08-20 19:28:21 +0200

    Modified ServiceNotFoundException.php

--- core/Exception/ServiceNotFoundException.php
+++ core/Exception/ServiceNotFoundException.php
@@ -6,7 +6,6 @@ use Exception;
 
 class ServiceNotFoundException extends Exception
 {
-
     public function __construct(string $service)
     {
         parent::__construct("Service with name '". $service ."' is not found!");
